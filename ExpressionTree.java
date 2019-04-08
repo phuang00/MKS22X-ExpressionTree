5a370f0c1c937eaa303a651185f1eb76654094c8
phuang00 <phuang00@stuy.edu>
2019-04-08 11:17:48 -0400

    wrote evaluate

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -30,7 +30,8 @@ public class ExpressionTree{
   /*return the value of the specified expression tree*/
   public double evaluate(){
     /*you are to write this method*/
-    return 0.0;
+    if (isValue()) return getValue();
+    return apply(getOp(), getLeft().evaluate(), getRight().evaluate());
     }
 
   /*use the correct operator on both a and b, and return that value*/
